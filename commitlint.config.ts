378223bad6af4705f2ebe4152fbafc62f42b31d6
Robson Oliveira dos Santos <robsonos@outlook.com>
2024-11-12 15:12:34 +0930

    build(chore): add scope rules to commit lint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,9 +1,18 @@
 import type { UserConfig } from '@commitlint/types';
+import { RuleConfigSeverity } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
-  formatter: '@commitlint/format',
+  rules: {
+    // Enforces specific scopes
+    'scope-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      ['chore', 'spie', 'spie-ui'],
+    ],
+    // Ensures that a scope is always provided
+    'scope-empty': [RuleConfigSeverity.Error, 'never'],
+  },
 };
 
 export default Configuration;
