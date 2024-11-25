35bb9d87889035335eba6a725936af3965e58a69
Robson Oliveira dos Santos <robsonos@outlook.com>
2024-11-25 12:15:50 +0930

    build(repo): fix and clarify production environment

--- apps/spie/src/app/app.ts
+++ apps/spie/src/app/app.ts
@@ -24,11 +24,7 @@ export default class App {
   );
 
   public static isDevelopmentMode(): boolean {
-    const isEnvironmentSet: boolean = 'ELECTRON_IS_DEV' in process.env;
-    const getFromEnvironment: boolean =
-      parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
-
-    return isEnvironmentSet ? getFromEnvironment : !environment.production;
+    return !environment.production;
   }
 
   private static onWindowAllClosed() {
