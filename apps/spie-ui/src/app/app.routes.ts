3704ffde18fc8ad3de0448331a5c8dc33d66f7a4
Robson Oliveira dos Santos <robsonos@outlook.com>
2024-12-01 15:33:35 +0930

    refactor(spie-ui): refactor routes

--- apps/spie-ui/src/app/app.routes.ts
+++ apps/spie-ui/src/app/app.routes.ts
@@ -4,17 +4,12 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent,
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full',
   },
 ];
